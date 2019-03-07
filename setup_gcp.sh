8b1cafcfb45f44c122478284367fba086a72a02f
Shi,ChengYou <45199828+uuboyscy@users.noreply.github.com>
2019-03-07 21:03:34 +0800

    Update setup_gcp.sh

--- setup_gcp.sh
+++ setup_gcp.sh
@@ -27,6 +27,7 @@ cd ~
 git clone https://github.com/uuboyscy/NobodyChatbot.git
 git clone https://github.com/BingHongLi/line_chat_bot_tutorial.git
 git clone https://github.com/uuboyscy/Chatbot_GCP.git
+git clone https://github.com/BingHongLi/fabric-appication-tuna-system.git
 sleep 5
 # change owner
 chown -R $username NobodyChatbot/
